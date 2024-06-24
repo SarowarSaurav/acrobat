5027309cdfa4be472aa6b5415f7abd43f40d1759
Sarowar Jahan Saurav <64561923+SarowarSaurav@users.noreply.github.com>
2024-06-24 02:29:35 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,66 @@
-from flask import Flask, jsonify
+import gradio as gr
+import difflib
 
-app = Flask(__name__)
+# Define the knowledge base as a dictionary
+knowledge_base = {
+    "CDA" : "Citizen Development Program",
+    "CDP" : "Citizen Development Program",
+    "ITSC" : "IT Service Continuity",
+    "GMAA" : "GMs Accountability Audit",
+    "GBCM" : "Global Business Continuity Management",
+    "PFP":"Pay for Performance",
+    "HORECA":"Hotel,Restaurants,Cafe",
+    "GT" : "General Trade",
+    "TLP":"Trade Loyalty Platform",
+    "MVP":"Most Valued Partners",
+    "VP":"Valued Partners",
+    "P":"Partners",
+    "OMR":"Open Market Rate","ZA04" :  "BAT South Africa - Heidelberg",
+    "ZLJM" :  "Zero Loss Journey Map",
+    "ZW06" :  "BAT Zimbabwe - Harare",
+    "SDLW" : "Same day last week",
+}
 
-@app.route("/")
-def root():
-    return jsonify({"Message": "Hello from Flask!"})
+# Define the Gradio interface function
+def chatbot_interface(acronym):
+    acronym = acronym.strip().toUpperCase()  # Remove leading/trailing whitespace and convert to uppercase
+    if acronym in knowledge_base:
+        response = f"Answer: {knowledge_base[acronym]}"
+    else:
+        closest_match = difflib.get_close_matches(acronym, knowledge_base.keys(), n=1)
+        if closest_match:
+            response = f"No exact match found in BATCCAPEDIA. Did you mean '{closest_match[0]}'?"
+        else:
+            response = "Not found in BATCCAPEDIA"
+    return response
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8000)
+# Create the Gradio interface
+css = """
+.gradio-container {
+    background: rgb(14, 43, 99);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+footer {
+    display: none !important;
+}
+"""
+
+# HTML content for the logo
+html_content = """
+<div style="text-align: center; margin-bottom: 20px;">
+    <img src="https://i.ibb.co/82Qf4rc/APMEA-CENTRAL-White.png" border="0" alt='BAT Bangladesh Logo' style='max-width: 300px;'>
+</div>
+"""
+
+with gr.Blocks(css=css) as demo:
+    gr.HTML(html_content)
+    gr.Interface(
+        fn=chatbot_interface,
+        inputs=gr.Textbox(label="Acronym", placeholder="Enter Acronym Here"),
+        outputs=gr.Textbox(label="Answer"),
+        theme='default',
+    )
+
+demo.launch()
